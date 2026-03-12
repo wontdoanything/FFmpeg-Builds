0fb78109b4dc123e45d6efb5c4b065f15e4daf8c
bxgms <542344410@qq.com>
2026-03-12 09:36:47 +0800

    【MOD】

--- scripts.d/45-libsmbclient.sh
+++ scripts.d/45-libsmbclient.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 ffbuild_enabled() {
+    [[ $TARGET == linux* ]] || return 1
     return 0
 }
 
 ffbuild_configure() {
-    echo --enable-libsmbclient
+    echo "--enable-libsmbclient"
 }
 
 ffbuild_dockerbuild() {
+    apt-get update
     apt-get install -y libsmbclient-dev
 }
