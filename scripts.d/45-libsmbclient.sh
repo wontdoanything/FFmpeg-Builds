546661aceb2b11c2b2e802ad57baeb230c64131c
bxgms <542344410@qq.com>
2026-03-12 11:52:40 +0800

    【MOD】关闭clone和本地编译

--- scripts.d/45-libsmbclient.sh
+++ scripts.d/45-libsmbclient.sh
@@ -5,11 +5,21 @@ ffbuild_enabled() {
     return 0
 }
 
+ffbuild_dockerbuild() {
+    apt-get update
+    apt-get install -y libsmbclient-dev
+}
+
 ffbuild_configure() {
     echo "--enable-libsmbclient"
 }
 
-ffbuild_dockerbuild() {
-    apt-get update
-    apt-get install -y libsmbclient-dev
+# 不需要下载源码
+ffbuild_clone() {
+    return 0
+}
+
+# 不需要编译
+ffbuild_build() {
+    return 0
 }
