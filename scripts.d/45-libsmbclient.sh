8169be694795f6d81b01468a17e83efc5b9a6c46
bxgms <542344410@qq.com>
2026-03-12 14:43:37 +0800

    【MOD】开启clone和本地编译到目标目录

--- scripts.d/45-libsmbclient.sh
+++ scripts.d/45-libsmbclient.sh
@@ -9,27 +9,19 @@ ffbuild_enabled() {
 }
 
 ffbuild_dockerbuild() {
-    apt-get update
-    apt-get install -y \
-      python3 \
-      python3-dev \
-      pkg-config \
-      libgnutls28-dev \
-      libacl1-dev \
-      libattr1-dev \
-      libpopt-dev
-
     ./configure \
       --prefix="$FFBUILD_PREFIX" \
-      --enable-fhs \
-      --disable-python \
-      --without-ad-dc
-
+      --disable-shared \
+      --enable-static \
+      --enable-libsmbclient
     make -j$(nproc)
-
-    make install
+    make install DESTDIR="$FFBUILD_DESTDIR"
 }
 
 ffbuild_configure() {
-    echo "--enable-libsmbclient"
+    echo --enable-libsmbclient
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libsmbclient
 }
